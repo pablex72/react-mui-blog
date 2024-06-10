63433b3fd738c497ceb0b6f28be4291d7b1cbdae
pablo <tonyp2hoy@gmail.com>
2024-06-10 13:23:54 -0400

    feat: init project setup material ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Button, Typography } from "@mui/material";
+import FacebookIcon from "@mui/icons-material/Facebook";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography
+        align="center"
+        variant="h1"
+        color={"tomato"}
+        sx={{ background: "red"}}
+      >
+        HELLLO
+      </Typography>
+      <Button startIcon={<FacebookIcon />} variant="outlined" color="secondary">
+        Text{" "}
+      </Button>
     </div>
   );
 }
